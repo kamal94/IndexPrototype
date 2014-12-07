47b4597519344d029519c468c958e4b342826323
kamal94 <kamal.aldin.94@gmail.com>
2014-12-07 12:52:54 -0500

    EDIT TO LAST COMMIT MESSAGE: Added sliding tab layout; changed ActionBar to Toolbar; changed the styles layout;added simple Network checking functionality.

--- app/src/main/java/indexprototype/com/kamal/indexprototype/MainActivity.java
+++ app/src/main/java/indexprototype/com/kamal/indexprototype/MainActivity.java
@@ -82,7 +82,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // as you specify a parent activity in AndroidManifest.xml..
         int id = item.getItemId();
         switch(item.getItemId()){
             case R.id.action_bar_settings:
